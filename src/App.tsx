94ac01067b64674662863b24eb5af20faf7954e9
Anibal <hello@anibalgatica.com>
2022-09-09 13:41:42 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center pb-32 w-full h-screen bg-slate-200">
+      <h1 className="text-4xl font-semibold">Hello simple world</h1>
+    </div>
+  );
+}
